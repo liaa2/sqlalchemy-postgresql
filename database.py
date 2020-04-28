8838bbf8c6c212f06e1fdae176d978ccb75055ee
liaa2 <36367920+liaa2@users.noreply.github.com>
2020-04-28 22:08:06 +1000

    add insert and save data

--- database.py
+++ database.py
@@ -13,3 +13,17 @@ class Database():
 
         for data in fetchQuery.fetchall():
             print(data)
+
+    def saveData(self, customer):
+        self.connection.execute(f"""INSERT INTO customer(name, age, email, address, zip_code) VALUES('{customer.name}', '{customer.age}', '{customer.email}', '{customer.address}', '{customer.zip_code}')""")
+
+
+class Customer():
+    def __init__(self, name, age, email, address, zip_code):
+        self.name = name
+        self.age = age
+        self.email = email
+        self.address = address
+        self.zip_code = zip_code
+
+
